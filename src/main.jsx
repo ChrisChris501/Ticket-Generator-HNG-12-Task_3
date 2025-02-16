b75f804690d4bad957e58677ff9b8d83d7572be4
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-02-16 00:32:10 +0000

    Fix routing issue with HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom"; 
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>
+);
