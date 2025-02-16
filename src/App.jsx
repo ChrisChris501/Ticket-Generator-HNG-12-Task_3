2c48a3b7f41d29e5f079acaccb9fcdb47b29342d
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-02-16 01:11:18 +0000

    Solved deployment bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import TicketForm from "./Components/TicketForm";
 import TicketConfirmation from "./Components/TicketConfirmation";
 import Ticket from "./Components/Ticket";
 
 function App() {
   return (
-    <div className="App bg-[#02191D] min-h-screen text-white">
-      <Routes>
-        <Route path="/" element={<Ticket />} />
-        <Route path="/Ticket" element={<Ticket />} />
-        <Route path="/TicketForm" element={<TicketForm />} />
-        <Route path="/TicketConfirmation" element={<TicketConfirmation />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="App bg-[#02191D] min-h-screen text-white">
+        <Routes>
+          <Route path="/" element={<Ticket />} />
+          <Route path="/Ticket" element={<Ticket />} />
+          <Route path="/TicketForm" element={<TicketForm />} />
+          <Route path="/TicketConfirmation" element={<TicketConfirmation />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
