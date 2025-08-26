8943fd1ddf0b3bc96f0aff3823964f3e84714b09
ChrisChris501 <Christanahagbekeye@gmail.com>
2025-08-26 16:05:12 +0100

    updating the set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import TicketForm from "./Components/TicketForm";
-import TicketConfirmation from "./Components/TicketConfirmation";
-import Ticket from "./Components/Ticket";
-import "./index.css";
+import './App.css'
 
 function App() {
+
   return (
-    <div className="App bg-[#02191D] min-h-screen text-white">
-      <Routes>
-        <Route path="/" element={<Ticket />} />
-        <Route path="/ticket" element={<Ticket />} />
-        <Route path="/ticketform" element={<TicketForm />} />
-        <Route path="/ticketconfirmation" element={<TicketConfirmation />} />
-      </Routes>
-    </div>
-  );
+      <div>
+        
+      </div>
+
+  )
 }
 
-export default App;
+export default App
